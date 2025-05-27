a1d826075221af7c57c81d1adf0862e8cfce9d23
Celestine Ntadi <contactilec@gmail.com>
2025-05-27 23:35:34 +0100

    DB endpoint

--- configs/db.js
+++ configs/db.js
@@ -4,11 +4,14 @@
 const { Pool } = require('pg');
 
 const isProduction = process.env.NODE_ENV === 'production';
-const shouldUseSSL = process.env.DATABASE_URL?.includes('sslmode=require') || isProduction;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: shouldUseSSL ? { rejectUnauthorized: false } : false,
+  ssl: isProduction
+    ? {
+        rejectUnauthorized: false, // Accept self-signed certs (CA should still be AWS RDS CA)
+      }
+    : false,
 });
 
 module.exports = pool;
